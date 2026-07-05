148ffc6361de809c3a7367545c08587adfc11283
pypynyaa <nikita662006@bk.ru>
2026-07-05 21:45:05 +0300

    Refine preloader logo ending

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -139,7 +139,7 @@ export default function Preloader() {
           <div className="site-preloader__wash" aria-hidden="true" />
           <div className="site-preloader__content">
             <div className="site-preloader__logo-wrap" aria-hidden="true">
-              <img className="site-preloader__logo" src="/logo.svg" width="109" height="67" alt="" />
+              <img className="site-preloader__logo" src="/preloader-logo.svg" width="109" height="67" alt="" />
             </div>
             <span className="site-preloader__sr">ITMO MEGABATTLE</span>
           </div>
